7a7b09f49ee236070633c980d28c65c3c2a254eb
cmccarthyirl <craig.mccarthy@live.ie>
2021-12-11 13:34:36 +0000

    Added Allure Reports and bumped Dependencies

--- wikipedia/src/test/java/com/cmccarthy/ui/step/Hooks.java
+++ wikipedia/src/test/java/com/cmccarthy/ui/step/Hooks.java
@@ -4,21 +4,11 @@ import com.cmccarthy.common.utils.HookUtil;
 import com.cmccarthy.ui.config.WikipediaAbstractTestDefinition;
 import com.cmccarthy.ui.utils.DriverManager;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.IAlterSuiteListener;
-import org.testng.IAnnotationTransformer;
-import org.testng.IConfigurationListener2;
-import org.testng.IExecutionListener;
-import org.testng.IInvokedMethodListener;
-import org.testng.IReporter;
-import org.testng.ITestListener;
 
-
-//public class Hooks extends WikipediaAbstractTestDefinition implements IConfigurationListener2, IExecutionListener, IAlterSuiteListener, IAnnotationTransformer, IInvokedMethodListener, ITestListener, IReporter {
 public class Hooks extends WikipediaAbstractTestDefinition {
     @Autowired
     private HookUtil hookUtil;
@@ -28,7 +18,6 @@ public class Hooks extends WikipediaAbstractTestDefinition {
     @Before
     public void beforeScenario(Scenario scenario) {
         driverManager.createDriver();
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
     }
 
     @After
