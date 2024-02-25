d3327cbe68d1a5a71747c24f7da649afeea0ecd3
Craig <craig.mccarthy@live.ie>
2024-02-26 02:07:35 +1100

    test latest build

--- scripts/downloadDriver.sh
+++ scripts/downloadDriver.sh
@@ -9,4 +9,4 @@ CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEA
 wget -N http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
-sudo mv -f chromedriver /home/runner/work/spring-cucumber-testng-parallel-test-harness/spring-cucumber-testng-parallel-test-harness/common/src/test/resources/drivers
+sudo mv -f chromedriver /home/runner/work/spring-cucumber-testng-parallel-test-harness/spring-cucumber-testng-parallel-test-harness/wikipedia/src/test/resources/drivers
