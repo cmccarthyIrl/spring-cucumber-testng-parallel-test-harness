448c6009a19e4a095710ed66a1afb3ca0efac0b5
Craig <craig.mccarthy@live.ie>
2024-03-02 00:25:36 +1100

    test latest build

--- .github/scripts/downloadDriver.sh
+++ .github/scripts/downloadDriver.sh
@@ -4,21 +4,12 @@ sudo apt-get install xvfb
 
 sudo apt-get install unzip
 
-#CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-#
-#wget -N http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
-#unzip chromedriver_linux64.zip
-#chmod +x chromedriver
-#sudo mv -f chromedriver /home/runner/work/spring-cucumber-testng-parallel-test-harness
-
-echo "pwd = $(pwd)"
-
 mkdir "temp" &&
 cd "temp" &&
 curl -L -k --output driver.zip https://www.nuget.org/api/v2/package/Selenium.WebDriver.ChromeDriver/ --ssl-no-revoke &&
 unzip driver.zip &&
 cd driver/linux64 &&
-cp "chromedriver" "$(pwd)/wikipedia/src/test/resources/drivers" &&
-chmod +700 "$(pwd)/wikipedia/src/test/resources/drivers/chromedriver" &&
+cp "chromedriver" "/home/runner/work/spring-cucumber-testng-parallel-test-harness/spring-cucumber-testng-parallel-test-harness/wikipedia/src/test/resources/drivers" &&
+chmod +700 "/home/runner/work/spring-cucumber-testng-parallel-test-harness/spring-cucumber-testng-parallel-test-harness/wikipedia/src/test/resources/drivers/chromedriver" &&
 cd ../../../ &&
 rm -rf temp
