648904d6ff88772be7669a5bf9eb6331e78dcb4f
Craig <craig.mccarthy@live.ie>
2024-03-02 23:43:54 +1100

    test latest

---
+++ .github/scripts/downloadDriver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install xvfb
+
+sudo apt-get install unzip
+
+ROOT_DIR=$(pwd)
+mkdir "temp" &&
+cd "temp" &&
+curl -L -k --output driver.zip https://www.nuget.org/api/v2/package/Selenium.WebDriver.ChromeDriver/ --ssl-no-revoke &&
+unzip driver.zip &&
+cd driver/linux64 &&
+chmod +x chromedriver &&
+sudo mv -f chromedriver "$ROOT_DIR/wikipedia/src/test/resources/drivers" &&
+sync -f "$ROOT_DIR/wikipedia/src/test/resources/drivers/chromedriver" &&
+cd ../../../ &&
+rm -rf temp
